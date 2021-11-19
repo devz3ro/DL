2a6f3ceeb93753605e4481f557ec778ce359abac
devz3ro <devz3ro@gmail.com>
2021-11-19 12:42:34 -0500

    Update mtv.rb

--- mtv.rb
+++ mtv.rb
@@ -19,26 +19,25 @@ mtv_list = mtv_url.split(" ")
 mtv_list.each do |url|
         system "yt-dlp #{url}"
         mtv_act_listing = Dir.each_child('.').grep(/\.mp4$/).sort_by! { |c| File.mtime(c) }
-        mtv_act_length = mtv_act_listing.length
         intermediate_file = 1
-        ts_list = []
-        concat = ""
-        for i in 1 .. mtv_act_length.to_i
-                unless i == mtv_act_length.to_i
-                        ts_list << i.to_s + ".ts|"
+        ts_file = []
+        ts_list = ""
+        for i in 1 .. mtv_act_listing.length
+                unless i == mtv_act_listing.length
+                        ts_file.push(i.to_s + ".ts|")
                 else
-                        ts_list << i.to_s + ".ts"
+                        ts_file.push(i.to_s + ".ts")
                 end
         end
-        ts_list.each do |transport_stream|
-                concat << transport_stream
+        ts_file.each do |ts_file_name|
+                ts_list.concat ts_file_name
         end
         episode_name = mtv_act_listing[0].split("_Act")
         mtv_act_listing.each do |mp4|
                 system "ffmpeg -i \"#{mp4}\" -c copy -bsf:v h264_mp4toannexb -f mpegts \"#{intermediate_file}.ts\""
                 intermediate_file += 1
         end
-        system "ffmpeg -i \"concat:#{concat}\" -c copy -bsf:a aac_adtstoasc -movflags faststart \"#{episode_name[0]}.mov\""
+        system "ffmpeg -i \"concat:#{ts_list}\" -c copy -bsf:a aac_adtstoasc -movflags faststart \"#{episode_name[0]}.mov\""
         FileUtils.rm Dir.glob('*.mp4')
         FileUtils.rm Dir.glob('*.ts')
 end
