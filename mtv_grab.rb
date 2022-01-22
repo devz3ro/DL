12ecc125a53158a2611c80f4ffdce2e26452828f
devz3ro <devz3ro@gmail.com>
2022-01-22 01:48:09 -0500

    Update mtv_grab.rb

--- mtv_grab.rb
+++ mtv_grab.rb
@@ -18,7 +18,7 @@ mtv_list = mtv_url.split(" ")
 
 mtv_list.each do |url|
         system "yt-dlp --verbose #{url}"
-        mtv_act_listing = Dir.each_child('.').grep(/\.mp4$/).sort_by! { |c| File.mtime(c) }
+        mtv_act_listing = Dir.each_child('.').grep(/\.mp4$/).sort_by { |item| item.to_s.split(/(\d+)/).map { |e| [e.to_i, e] } }
         intermediate_file = 1
         ts_file = []
         ts_list = ""
